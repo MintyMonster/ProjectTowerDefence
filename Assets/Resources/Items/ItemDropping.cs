9934e6945d128989432f97b9170fb1ef17f0686d
MintyMonster <camerongliddell@gmail.com>
2022-03-22 04:23:00 +0000

    Main Menu fixes
    - Added logo splash screen
    - Added falling menu objects
    - Added logo for game

---
+++ Assets/Resources/Items/ItemDropping.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ItemDropping : MonoBehaviour
+{
+    private GameObject[] spawnPoints;
+    private GameObject[] items;
+    private float spawnTime = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("Spawner");
+        items = Resources.LoadAll<GameObject>("Menu_Prefabs").Cast<GameObject>().ToArray();
+
+
+        InvokeRepeating("SpawnRandom", spawnTime, spawnTime);
+    }
+
+    private void SpawnRandom()
+    {
+
+        GameObject sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        GameObject i = items[Random.Range(0, items.Length)];
+
+        GameObject.Instantiate(i, sp.transform.position, Quaternion.identity);
+    }
+}
